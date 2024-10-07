2f9ea0b7fbb0ca992e85c23c3e8151cd6bf7a8cd
Saroj Thapa <sarojthapa199@gmail.com>
2024-10-07 21:03:46 +0545

    adjusted cookie settings

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -6,6 +6,9 @@ export const generateToken = (user, message, statusCode, res) => {
             Date.now() + process.env.COOKIE_EXPIRES * 24 * 60 * 60 * 1000
          ),
          httpOnly: true,
+         secure: process.env.NODE_ENV === "production",
+         sameSite: "None",
+         path: "/",
       })
       .json({
          success: true,
